cc2425c7032b03b9a483da6799e1b7c400b65154
Davi Ramos <137097142+terem7@users.noreply.github.com>
2024-11-26 00:29:05 -0300

    Initial commit

---
+++ desafios/dsf002.py
@@ -0,0 +1,5 @@
+dia = input('Qual dia voce nasceu?')
+mes = input('Qual mes voce nasceu?')
+ano = input('Qual ano voce nasceu?')
+
+print('Voce nasceu no dia',dia,'de',mes,'de',ano,'. correto?' )
---
+++ desafios/dsf003.py
@@ -0,0 +1,6 @@
+n1 = int(input('Primeiro numero '))
+n2 = int(input('Segundo numero '))
+
+s = n1 + n2
+
+print('a soma entre {n1} e {n2} e igual a {s}'.format(n1, n2, s))
