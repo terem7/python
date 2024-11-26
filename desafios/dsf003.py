3fb206a063d987b57c6b904827e5bd5501beeba7
Davi Ramos <137097142+terem7@users.noreply.github.com>
2024-11-26 04:02:54 -0300

    Update dsf003.py

--- desafios/dsf003.py
+++ desafios/dsf003.py
@@ -1,6 +1,6 @@
-n1 = float(input('Primeiro numero '))
-n2 = float(input('Segundo numero '))
+n1 = int(input('Primeiro numero '))
+n2 = int(input('Segundo numero '))
 
 s = n1 + n2
 
-print('a soma entre {0} e {1} e igual a {2}'.format(n1, n2, s))
+print('a soma entre {} e {} e igual a {}'.format(n1, n2, s))
